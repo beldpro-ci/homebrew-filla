7fec04df6408010a12c60f57adae3f8e59ad399f
beldpro <beldpro@gmail.com>
2017-05-21 00:43:31 -0300

    Adds formula

---
+++ Formula/release.rb
@@ -0,0 +1,13 @@
+class Goreleaser < Formula
+  desc "File uploading from the terminal"
+  homepage "https://filla.be"
+  url "https://releases.filla.be/darwin-amd64/filla.tgz"
+  version "0.1.0"
+  sha256 "62c90f7501cdd08ae7d6f864f8481137b37c123a740929e21d431823315aaca0"
+  
+  depends_on "git"
+
+  def install
+    bin.install "filla"
+  end
+end
