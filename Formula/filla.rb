bce5a1990627300748258c44327e9033fa314614
beldpro <beldpro@gmail.com>
2017-05-21 00:49:20 -0300

    Removes `git` depends on

--- Formula/filla.rb
+++ Formula/filla.rb
@@ -4,8 +4,6 @@ class Filla < Formula
   url "https://releases.filla.be/darwin-amd64/filla.tgz"
   version "0.1.0"
   sha256 "62c90f7501cdd08ae7d6f864f8481137b37c123a740929e21d431823315aaca0"
-  
-  depends_on "git"
 
   def install
     bin.install "filla"
